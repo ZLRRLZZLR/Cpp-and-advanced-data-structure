4069a798cf2318bd3ece1d8e623d71373e720bdc
zhou-liurong <19551230598@163.com>
2024-07-15 22:50:32 +0800

    Initial learning of C++

---
+++ cpp/cpp/test.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    cout << "     **     " << endl;
+//    cout << "     **     " << endl;
+//    cout << "************" << endl;
+//    cout << "************" << endl;
+//    cout << "    *  *    " << endl;
+//    cout << "    *  *    " << endl;
+//
+//}
+// 64 位输出请用 printf("%lld")
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    cout << "Hello Nowcoder!" << endl;
+//}
+//// 64 位输出请用 printf("%lld")
