900f62eeb7ae930c22d3db84b135fdbc44403294
zhou-liurong <19551230598@163.com>
2024-10-06 23:08:26 +0800

    Class related topics

--- 1.cpp/1.cpp/Test.cpp
+++ 1.cpp/1.cpp/Test.cpp
@@ -1,18 +1,1377 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+using namespace std;
+
 #include<iostream>
 using namespace std;
 
-class Date {
+
+class Date
+{
+
+
 public:
-	Date
+
+
+	// 获取某年某月的天数
+
+
+	int GetMonthDay(int year, int month) {
+		static int arr[13] = { -1,31,28,31,30,31,
+			30,31,31,20,31,30,31 };
+
+
+		if (month == 2 &&
+			year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+			return 29;
+		}
+		return arr[month];
+
+
+	}
+
+	Date(int num) {
+		_year = num / 10000;
+		_day = num % 100;
+		num = num / 100;
+
+		_month = num % 100;
+	}
+	// 拷贝构造函数
+	// d2(d1)
+
+
+	Date(const Date& d) {
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
+
+
+	}
+
+
+	// 赋值运算符重载
+
+	Date& operator=(const Date& d) {
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
+
+
+		return *this;
+	}
+
+
+	// 日期+=天数
+
+
+	Date& operator+=(int day);
+
+	// 日期+天数
+
+
+	Date operator+(int day);
+
+
+	// 日期-=天数
+
+
+	Date& operator-=(int day);
+
+
+	// 日期-天数
+
+
+	Date operator-(int day);
+
+
+	// 前置++
+
+
+	Date& operator++();
+
+
+	// 后置++
+
+
+	Date operator++(int);
+
+	// 前置--
+
+
+	Date& operator--();
+
+
+	// 后置--
+
+
+	Date operator--(int);
+
+
+	// >运算符重载
+
+
+	bool operator>(const Date& d)const;
+
+	// ==运算符重载
+
+
+	bool operator==(const Date& d)const;
+
+
+	// >=运算符重载
+
+
+	bool operator >= (const Date& d)const;
+
+
+	// <运算符重载
+
+
+	bool operator < (const Date& d)const;
+
+
+	// <=运算符重载
+
+
+	bool operator <= (const Date& d)const;
+
+
+	// !=运算符重载
+
+
+	bool operator != (const Date& d)const;
+
+	// 日期-日期 返回天数
+
+
+	int operator-(const Date& d);
+
+
+private:
+
+
+	int _year;
+
+
+	int _month;
+
+
+	int _day;
+
+
 };
 
 
+// 日期+=天数
+Date& Date::operator+=(int day) {
+	if (day < 0) {
+		return *this -= (-day);
+	}
+	_day += day;
+	while (_day > GetMonthDay(_year, _month)) {
+		_day -= GetMonthDay(_year, _month);
+		_month++;
+		if (13 == _month) {
+			_year++;
+			_month = 1;
+		}
+
+
+	}
+	return *this;
+}
+
+
+// 日期+天数
+
+
+Date Date::operator+(int day) {
+	Date tmp = *this;
+	tmp += day;
+
+
+	return tmp;
+}
+
+
+// 前置++
+
+
+Date& Date::operator++() {
+	*this = *this + 1;
+
+
+	return *this;
+}
+
+
+
+
+// 后置++
+
+
+Date Date::operator++(int) {
+	Date tmp = *this;
+	++(*this);
+	return tmp;
+}
+
+
+
+
+// 日期-=天数
+
+
+Date& Date::operator-=(int day) {
+	if (day < 0) {
+		return *this += (-day);
+	}
+	_day -= day;
+	while (_day <= 0) {
+		if (1 == _month) {
+			_year--;
+			_month = 12;
+			_day += GetMonthDay(_year, _month);
+		}
+		else {
+			_month--;
+			_day += GetMonthDay(_year, _month);
+		}
+	}
+
+
+	return *this;
+}
+
+
+// 日期-天数
+
+
+Date Date::operator-(int day) {
+	Date tmp = *this;
+	tmp -= day;
+
+
+	return tmp;
+}
+
+
+
+
+// 前置--
+
+
+Date& Date::operator--() {
+	*this = *this - 1;
+
+
+	return *this;
+}
+
+
+// 后置--
+
+
+Date Date::operator--(int) {
+	Date tmp = *this;
+	--(*this);
+	return tmp;
+}
 
 
 
 
 
+
+// >运算符重载
+
+
+bool Date::operator>(const Date& d) const {
+	if (_year > d._year) {
+		return true;
+	}
+	else if (_year == d._year) {
+		if (_month > d._month) {
+			return true;
+		}
+		else if (_month == d._month) {
+			if (_day > d._day) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+	else {
+		return false;
+	}
+}
+
+
+
+
+
+
+// ==运算符重载
+
+
+bool Date::operator==(const Date& d) const {
+	return _year == d._year && _month == d._month && _day == d._day;
+}
+
+
+
+
+
+
+// >=运算符重载
+
+
+bool Date::operator >= (const Date& d) const {
+	return *this > d || *this == d;
+}
+
+
+
+
+
+
+// <运算符重载
+
+
+bool Date::operator < (const Date& d) const {
+	return !(*this >= d);
+}
+
+
+
+
+
+
+// <=运算符重载
+
+
+bool Date::operator <= (const Date& d) const {
+	return !(*this > d);
+}
+
+
+
+
+
+
+// !=运算符重载
+
+
+bool Date::operator != (const Date& d) const {
+	return !(*this == d);
+}
+
+
+
+
+
+
+// 日期-日期 返回天数
+
+
+int Date::operator-(const Date& d) {
+	int flag = 1;
+	int day = 0;
+	Date max = *this;
+	Date min = d;
+	if (*this < d) {
+		flag = -1;
+		min = *this;
+		max = d;
+	}
+	while (min < max) {
+		++min;
+		++day;
+	}
+
+
+	return day * flag;
+}
+
+
+
+
+	int main() {
+		int num1, num2;
+		while (cin >> num1 >> num2) { // 注意 while 处理多个 case
+			Date n1(num1);
+			Date n2(num2);
+			cout << (n2 - n1 + 1) << endl;
+
+		}
+
+		return 0;
+	}
+	// 64 位输出请用 printf("%lld")
+
+//class Date
+//{
+//
+//
+//public:
+//
+//
+//	// 获取某年某月的天数
+//
+//
+//	int GetMonthDay(int year, int month) {
+//		static int arr[13] = { -1,31,28,31,30,31,
+//			30,31,31,20,31,30,31 };
+//
+//
+//		if (month == 2 &&
+//			year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+//			return 29;
+//		}
+//		return arr[month];
+//
+//
+//	}
+//
+//
+//
+//
+//
+//
+//	// 全缺省的构造函数
+//
+//
+//	Date(int year = 1900, int month = 1, int day = 1) {
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//
+//
+//	}
+//
+//
+//
+//
+//
+//
+//	// 拷贝构造函数
+//	// d2(d1)
+//
+//
+//	Date(const Date& d) {
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//
+//
+//	}
+//
+//
+//
+//
+//
+//
+//	// 赋值运算符重载
+//
+//
+//  // d2 = d3 -> d2.operator=(&d2, d3)
+//
+//
+//	Date& operator=(const Date& d) {
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//
+//
+//		return *this;
+//	}
+//
+//
+//
+//
+//	// 日期+=天数
+//
+//
+//	Date& operator+=(int day);
+//
+//
+//
+//
+//	// 日期+天数
+//
+//
+//	Date operator+(int day);
+//
+//
+//	// 日期-=天数
+//
+//
+//	Date& operator-=(int day);
+//
+//
+//	// 日期-天数
+//
+//
+//	Date operator-(int day);
+//
+//
+//
+//
+//	// 前置++
+//
+//
+//	Date& operator++();
+//
+//
+//
+//
+//
+//
+//	// 后置++
+//
+//
+//	Date operator++(int);
+//
+//
+//
+//
+//
+//
+//	// 前置--
+//
+//
+//	Date& operator--();
+//
+//
+//	// 后置--
+//
+//
+//	Date operator--(int);
+//
+//
+//
+//
+//
+//
+//	// >运算符重载
+//
+//
+//	bool operator>(const Date& d)const;
+//
+//
+//
+//
+//
+//
+//	// ==运算符重载
+//
+//
+//	bool operator==(const Date& d)const;
+//
+//
+//
+//
+//
+//
+//	// >=运算符重载
+//
+//
+//	bool operator >= (const Date& d)const;
+//
+//
+//
+//
+//
+//
+//	// <运算符重载
+//
+//
+//	bool operator < (const Date& d)const;
+//
+//
+//
+//
+//
+//
+//	// <=运算符重载
+//
+//
+//	bool operator <= (const Date& d)const;
+//
+//
+//
+//
+//
+//
+//	// !=运算符重载
+//
+//
+//	bool operator != (const Date& d)const;
+//
+//
+//
+//
+//
+//
+//	// 日期-日期 返回天数
+//
+//
+//	int operator-(const Date& d);
+//
+//
+//private:
+//
+//
+//	int _year;
+//
+//
+//	int _month;
+//
+//
+//	int _day;
+//
+//
+//};
+//
+//
+//// 日期+=天数
+//Date& Date::operator+=(int day) {
+//	if (day < 0) {
+//		return *this -= (-day);
+//	}
+//	_day += day;
+//	while (_day > GetMonthDay(_year, _month)) {
+//		_day -= GetMonthDay(_year, _month);
+//		_month++;
+//		if (13 == _month) {
+//			_year++;
+//			_month = 1;
+//		}
+//
+//
+//	}
+//	return *this;
+//}
+//
+//
+//// 日期+天数
+//
+//
+//Date Date::operator+(int day) {
+//	Date tmp = *this;
+//	tmp += day;
+//
+//
+//	return tmp;
+//}
+//
+//
+//// 前置++
+//
+//
+//Date& Date::operator++() {
+//	*this = *this + 1;
+//
+//
+//	return *this;
+//}
+//
+//
+//
+//
+//// 后置++
+//
+//
+//Date Date::operator++(int) {
+//	Date tmp = *this;
+//	++(*this);
+//	return tmp;
+//}
+//
+//
+//
+//
+//// 日期-=天数
+//
+//
+//Date& Date::operator-=(int day) {
+//	if (day < 0) {
+//		return *this += (-day);
+//	}
+//	_day -= day;
+//	while (_day <= 0) {
+//		if (1 == _month) {
+//			_year--;
+//			_month = 12;
+//			_day += GetMonthDay(_year, _month);
+//		}
+//		else {
+//			_month--;
+//			_day += GetMonthDay(_year, _month);
+//		}
+//	}
+//
+//
+//	return *this;
+//}
+//
+//
+//// 日期-天数
+//
+//
+//Date Date::operator-(int day) {
+//	Date tmp = *this;
+//	tmp -= day;
+//
+//
+//	return tmp;
+//}
+//
+//
+//
+//
+//// 前置--
+//
+//
+//Date& Date::operator--() {
+//	*this = *this - 1;
+//
+//
+//	return *this;
+//}
+//
+//
+//// 后置--
+//
+//
+//Date Date::operator--(int) {
+//	Date tmp = *this;
+//	--(*this);
+//	return tmp;
+//}
+//
+//
+//
+//
+//
+//
+//// >运算符重载
+//
+//
+//bool Date::operator>(const Date& d) const {
+//	if (_year > d._year) {
+//		return true;
+//	}
+//	else if (_year == d._year) {
+//		if (_month > d._month) {
+//			return true;
+//		}
+//		else if (_month == d._month) {
+//			if (_day > d._day) {
+//				return true;
+//			}
+//			else {
+//				return false;
+//			}
+//		}
+//		else {
+//			return false;
+//		}
+//	}
+//	else {
+//		return false;
+//	}
+//}
+//
+//
+//
+//
+//
+//
+//// ==运算符重载
+//
+//
+//bool Date::operator==(const Date& d) const {
+//	return _year == d._year && _month == d._month && _day == d._day;
+//}
+//
+//
+//
+//
+//
+//
+//// >=运算符重载
+//
+//
+//bool Date::operator >= (const Date& d) const {
+//	return *this > d || *this == d;
+//}
+//
+//
+//
+//
+//
+//
+//// <运算符重载
+//
+//
+//bool Date::operator < (const Date& d) const {
+//	return !(*this >= d);
+//}
+//
+//
+//
+//
+//
+//
+//// <=运算符重载
+//
+//
+//bool Date::operator <= (const Date& d) const {
+//	return !(*this > d);
+//}
+//
+//
+//
+//
+//
+//
+//// !=运算符重载
+//
+//
+//bool Date::operator != (const Date& d) const {
+//	return !(*this == d);
+//}
+//
+//
+//
+//
+//
+//
+//// 日期-日期 返回天数
+//
+//
+//int Date::operator-(const Date& d) {
+//
+//	int flag = 1;
+//	int day = 0;
+//	Date max = *this;
+//	Date min = d;
+//	if (*this < d) {
+//		flag = -1;
+//		min = *this;
+//		max = d;
+//	}
+//	while (min < max) {
+//		++min;
+//		++day;
+//	}
+//
+//
+//	return day * flag;
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//class Date {
+//public:
+//    int GetNum(int year, int month) {
+//        static int arr[13] = { -1,31,28,31,30,31,
+//        30,31,31,30,31,30,31 };
+//        if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
+//            return 29;
+//        }
+//        return arr[month];
+//    }
+//    int Sum(int year, int month, int day) {
+//        int sum = 0;
+//        for (int m = 1; m < month; m++) {
+//            sum += GetNum(year, m);
+//        }
+//        sum += day;
+//        return sum;
+//    }
+//};
+//
+//
+//
+//int main() {
+//    int year, month, day;
+//    while (cin >> year >> month >> day) { // 注意 while 处理多个 case
+//        cout << Date().Sum(year, month, day) << endl;
+//    }
+//
+//    return 0;
+//}
+// 64 位输出请用 printf("%lld")
+//class Solution {
+//private:
+//    static int _i;
+//    static int _sum;
+//    class Sum {
+//    public:
+//        Sum() {
+//            _sum += _i;
+//            ++_i;
+//        }
+//    };
+//
+//
+//public:
+//    int Sum_Solution(int n) {
+//        Sum arr[n];
+//        return _sum;
+//    }
+//
+//
+//
+//};
+//
+//int Solution::_i = 1;
+//int Solution::_sum = 0;
+
+//class A {
+//public:
+//	A(int a = 0)
+//		:_a(a)
+//	{
+//		cout << "A(int a)" << endl;
+//	}
+//	~A() {
+//		cout << "~A()" << endl;
+//	}
+//private:
+//	int _a;
+//};
+//
+//class solution {
+//public:
+//	int Sum_Solution(int n) {
+//		return n;
+//	}
+//};
+//
+//bool myfunction(int i, int j) { return (i > j); }
+//
+//int main() {
+//	A aa;
+//	A();
+//	A(1);
+//	solution st;
+//	cout << st.Sum_Solution(10) << endl;
+//
+//	cout << solution().Sum_Solution(10) << endl;
+//
+//	int a[] = { 32,71,12,45,26,80,53,33 };
+//
+//	sort(a, a + 8, greater<int>());
+//
+//}
+//class A
+//{
+//public:
+//	A(int a = 0)
+//		: _a(a)
+//	{
+//		cout << "A():" << this << endl;
+//	}
+//	~A()
+//	{
+//		cout << "~A():" << this << endl;
+//	}
+//private:
+//	int _a;
+//};
+//// 定位new/replacement new
+//int main()
+//{
+//	// p1现在指向的只不过是与A对象相同大小的一段空间，还不能算是一个对象，因为构造函数没
+//	有执行
+//		A* p1 = (A*)malloc(sizeof(A));
+//	new(p1)A; // 注意：如果A类的构造函数有参数时，此处需要传参
+//	p1->~A();
+//	free(p1);
+//	A* p2 = (A*)operator new(sizeof(A));
+//	new(p2)A(10);
+//	p2->~A();
+//	operator delete(p2);
+//	return 0;
+//}
+
+//class A
+//{
+//public:
+//	A(int a = 0)
+//		: _a(a)
+//	{
+//		cout << "A():" << this << endl;
+//	}
+//	~A()
+//	{
+//		cout << "~A():" << this << endl;
+//	}
+//private:
+//	int _a;
+//};
+//// 定位new/replacement new
+//int main()
+//{
+//	// p1现在指向的只不过是与A对象相同大小的一段空间，还不能算是一个对象，因为构造函数没
+//	有执行
+//		A* p1 = (A*)malloc(sizeof(A));
+//	new(p1)A; // 注意：如果A类的构造函数有参数时，此处需要传参
+//	p1->~A();
+//	free(p1);
+//	A* p2 = (A*)operator new(sizeof(A));
+//	new(p2)A(10);
+//	p2->~A();
+//	operator delete(p2);
+//	return 0;
+//}
+
+//class A
+//{
+//public:
+//	A(int a = 0)
+//		: _a(a)
+//	{
+//		cout << "A():" << this << endl;
+//	}
+//	~A()
+//	{
+//		cout << "~A():" << this << endl;
+//	}
+//private:
+//	int _a;
+//};
+//// 定位new/replacement new
+//int main()
+//{
+//	// p1现在指向的只不过是与A对象相同大小的一段空间，还不能算是一个对象，因为构造函数没
+//	有执行
+//		A* p1 = (A*)malloc(sizeof(A));
+//	new(p1)A; // 注意：如果A类的构造函数有参数时，此处需要传参
+//	p1->~A();
+//	free(p1);
+//	A* p2 = (A*)operator new(sizeof(A));
+//	new(p2)A(10);
+//	p2->~A();
+//	operator delete(p2);
+//	return 0;
+//}
+
+
+///*
+//operator new：该函数实际通过malloc来申请空间，当malloc申请空间成功时直接返回；申请空间
+//失败，尝试执行空 间不足应对措施，如果改应对措施用户设置了，则继续申请，否
+//则抛异常。
+//*/
+//void* __CRTDECL operator new(size_t size) _THROW1(_STD bad_alloc)
+//{
+//	// try to allocate size bytes
+//	void* p;
+//	while ((p = malloc(size)) == 0)
+//		if (_callnewh(size) == 0)
+//		{
+//			// report no memory
+//			// 如果申请内存失败了，这里会抛出bad_alloc 类型异常
+//			static const std::bad_alloc nomem;
+//			_RAISE(nomem);
+//		}
+//	return (p);
+//}
+///*
+//operator delete: 该函数最终是通过free来释放空间的
+//*/
+//void operator delete(void* pUserData)
+//{
+//	_CrtMemBlockHeader* pHead;
+//	RTCCALLBACK(_RTC_Free_hook, (pUserData, 0));
+//	if (pUserData == NULL)
+//		return;
+//	_mlock(_HEAP_LOCK); /* block other threads */
+//	__TRY
+//		/* get a pointer to memory block header */
+//		pHead = pHdr(pUserData);
+//	/* verify block type */
+//	_ASSERTE(_BLOCK_TYPE_IS_VALID(pHead->nBlockUse));
+//	_free_dbg(pUserData, pHead->nBlockUse);
+//	__FINALLY
+//		_munlock(_HEAP_LOCK); /* release other threads */
+//	__END_TRY_FINALLY
+//		return;
+//}
+///*
+//free的实现
+//*/
+//#define free(p) _free_dbg(p, _NORMAL_BLOCK)
+
+
+//class A
+//{
+//public:
+//	A(int a = 0)
+//		: _a(a)
+//	{
+//		cout << "A():" << this << endl;
+//	}
+//	~A()
+//	{
+//		cout << "~A():" << this << endl;
+//	}
+//private:
+//	int _a;
+//};
+//int main()
+//{
+//	// new/delete 和 malloc/free最大区别是 new/delete对于【自定义类型】除了开空间
+//	还会调用构造函数和析构函数
+//		A* p1 = (A*)malloc(sizeof(A));
+//	A* p2 = new A(1);
+//	free(p1);
+//	delete p2;
+//	// 内置类型是几乎是一样的
+//	int* p3 = (int*)malloc(sizeof(int)); // C
+//	int* p4 = new int;
+//	free(p3);
+//	delete p4;
+//	A* p5 = (A*)malloc(sizeof(A) * 10);
+//	A* p6 = new A[10];
+//	free(p5);
+//	delete[] p6;
+//	return 0;
+//}
+
+
+//void Test()
+//{
+//	// 动态申请一个int类型的空间
+//	int* ptr4 = new int;
+//	// 动态申请一个int类型的空间并初始化为10
+//	int* ptr5 = new int(10);
+//	// 动态申请10个int类型的空间
+//	int* ptr6 = new int[3];
+//	delete ptr4;
+//	delete ptr5;
+//	delete[] ptr6;
+//}
+
+//void Test()
+//{
+//	// 1.malloc/calloc/realloc的区别是什么？
+//	int* p2 = (int*)calloc(4, sizeof(int));
+//	int* p3 = (int*)realloc(p2, sizeof(int) * 10);
+//	// 这里需要free(p2)吗？
+//	free(p3);
+//}
+//int globalVar = 1;
+//static int staticGlobalVar = 1;
+//void Test()
+//{
+//	static int staticVar = 1;
+//	int localVar = 1;
+//	int num1[10] = { 1, 2, 3, 4 };
+//	char char2[] = "abcd";
+//	const char* pChar3 = "abcd";
+//	int* ptr1 = (int*)malloc(sizeof(int) * 4);
+//	int* ptr2 = (int*)calloc(4, sizeof(int));
+//	int* ptr3 = (int*)realloc(ptr2, sizeof(int) * 4);
+//	free(ptr1);
+//	free(ptr3);
+//}
+//class A {
+//public:
+//	A() {
+//		++_scount;
+//	}
+//	A(const A& t) {
+//		++_scount;
+//	}
+//	~A() {
+//		--_scount;
+//	}
+//	static int GetACount() {
+//		_a++;
+//		return _scount;
+//	}
+//
+//	void func() {
+//		cout << _scount << endl;
+//		cout << GetACount() << endl;
+//	}
+//private:
+//	static int _scount;
+//
+//	int _a = 1;
+//
+//	int _a = 1;
+//};
+//
+//int A::_scount = 0;
+//int main() {
+//	cout << A::_scount << endl;
+//	cout << sizeof(A) << endl;
+//
+//	cout << A::GetACount() << endl;
+//	A a1, a2;
+//	{
+//		A a3(a1);
+//		cout << A::GetACount() << endl;
+//	}
+//
+//	cout << A::GetACount() << endl;
+//	cout << a1::GetACount() << endl;
+//
+//	return 0;
+//}
+//
+//class B;
+//
+//class A {
+//	friend void func(const A& aa, const B& bb);
+//private:
+//	int _b1 = 3;
+//	int _b2 = 4;
+//};
+//
+//void func(const A& aa, const B& bb) {
+//	cout << aa._a1 << endl;
+//	cout << bb._b1 << endl;
+//}
+//int main() {
+//	A aa;
+//	B bb;
+//	func(aa, bb);
+//
+//	return 0;
+//}
+//
+//class A
+
+
+
+
+//class Date {
+//public:
+//	Date(int year = 1, int month = 1, int day = 1)
+//		:_year(year)
+//		, _month(month)
+//	{}
+//	void Print()const {
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//private:
+//	int _year = 1;
+//	int _month = 1;
+//	int _day;
+//
+//	int* _ptr = (int*)mallo(12);
+//	Time _t = 1;
+//};
+//
+//int main() {
+//	Date d1(2024, 7, 14);
+//	d1.Print();
+//
+//	return 0;
+//}
+
+
+
+
+
+//class Time {
+//public:
+//	Time(int hour)
+//		:_hour(hour) {
+//		cout << "Time()" << endl;
+//	}
+//private:
+//	int _hour;
+//};
+//
+//class Date {
+//public:
+//	Date(int year, int month, int day)
+//		:_year(year)
+//		, _month(month)
+//		, _day(day)
+//		{
+//		}
+//	void Print() const {
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//int main() {
+//	int x = 0;
+//	Date d1(x, 2024, 7, 14);
+//	d1.Print();
+//	const int x = 1;
+//
+//}
+
+
+//class Date {
+//public:
+//	Date(int year = 1, int month = 1; , int day = 1) {
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//	Date& operator=(const Date& d) {
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//		
+//		return *this;
+//	}
+//	Date(const Date& d) {
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//	}
+//	Date(const Date& d) {
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//	}
+//	Date()
+//};
+
+
+
+
+
+
+//class A {
+//public:
+//	void func() {
+//		cout << "A:func" << endl;
+//	}
+//
+//};
+//typedef void(A::* PF)();
+//int main{
+//	void(A:: * pf)() = nullptr;
+//PF pf = nulllptr;
+//
+//pf = &A::func;
+//A aa;
+//(aa.*pf)();
+//}
 
 //typedef int STDataType;
 //
