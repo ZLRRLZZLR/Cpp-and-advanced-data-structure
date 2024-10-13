14fdfeead66b7e2eb4c0639391df7dbb586f6dd1
zhou-liurong <19551230598@163.com>
2024-10-13 23:25:25 +0800

    daily work

--- 2.cpp/2.cpp/源.cpp
+++ 2.cpp/2.cpp/源.cpp
@@ -5,6 +5,146 @@
 #include<list>
 using namespace std;
 
+
+void test_string5()
+{
+	string s("hello world");
+	s.push_back(' ');
+	s.push_back('x');
+	s.append("yyyyyy");
+
+	cout << s << endl;
+
+	s += ' ';
+	s += "33333333333";
+	cout << s << endl;
+
+	s.insert(0, "hello bit ");
+	cout << s << endl;
+
+	s.insert(10, "zzzz");
+	cout << s << endl;
+
+	s.insert(0, "p");
+	cout << s << endl;
+
+	char ch = 't';
+	s.insert(0, 1, ch);
+	s.insert(s.begin(), ch);
+
+	cout << s << endl;
+}
+
+void test_string6()
+{
+	string s("hello world");
+	s.erase(6, 1);
+	cout << s << endl;
+
+	// 头删
+	s.erase(0, 1);
+	cout << s << endl;
+
+	s.erase(s.begin());
+	cout << s << endl;
+
+	// 尾删
+	s.erase(--s.end());
+	cout << s << endl;
+
+	s.erase(s.size() - 1, 1);
+	cout << s << endl;
+
+	string ss("hello world");
+	ss.erase(6);
+	cout << ss << endl;
+
+	string sss("hello                 world hello bit");
+	/*size_t pos = sss.find(' ');
+	while (pos != string::npos)
+	{
+		sss.replace(pos, 1, "%%");
+		cout << sss << endl;
+
+		pos = sss.find(' ', pos+2);
+	}
+	cout << sss << endl;*/
+
+	//sss.replace(5, 1, "%%");
+	//cout << sss << endl;
+
+	string tmp;
+	tmp.reserve(sss.size());
+	for (auto ch : sss)
+	{
+		if (ch == ' ')
+			tmp += "%%";
+		else
+			tmp += ch;
+	}
+	cout << tmp << endl;
+	//sss = tmp;
+	sss.swap(tmp);
+	cout << sss << endl;
+
+	string file;
+	cin >> file;
+	FILE* fout = fopen(file.c_str(), "r");
+	char ch = fgetc(fout);
+	while (ch != EOF)
+	{
+		cout << ch;
+		ch = fgetc(fout);
+	}
+	fclose(fout);
+}
+
+void SplitFilename(const std::string& str)
+{
+	std::cout << "Splitting: " << str << '\n';
+	std::size_t found = str.find_last_of("/\\");
+
+	std::cout << " path: " << str.substr(0, found) << '\n';
+	std::cout << " file: " << str.substr(found + 1) << '\n';
+}
+// 16：10
+void test_string7()
+{
+	string s("test.cpp.zip");
+	size_t pos = s.rfind('.');
+	string suffix = s.substr(pos);
+	cout << suffix << endl;
+
+	std::string str("Please, replace the vowels in this sentence by asterisks.");
+	std::cout << str << '\n';
+
+	std::size_t found = str.find_first_not_of("abcdef");
+	while (found != std::string::npos)
+	{
+		str[found] = '*';
+		found = str.find_first_not_of("abcdef", found + 1);
+	}
+
+	std::cout << str << '\n';
+
+	std::string str1("/usr/bin/man");
+	std::string str2("D:\\1-教学服务团队\\1-比特课件\\4.C++课件\\C++课件V6-2022年修订\\C++课件--2022修订\\C++进阶课件");
+
+	SplitFilename(str1);
+	SplitFilename(str2);
+}
+
+void test_string8()
+{
+	string s1("hello");
+
+	string s2 = s1 + "world";
+	cout << s2 << endl;
+
+	string s3 = "world" + s1;
+	cout << s3 << endl;
+}
+
 //for(auto& e : array)
 //e*= 2 = 0;
 //for (auto e : array)
