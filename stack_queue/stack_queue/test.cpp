afa2bcc52c7a04d171dc946a850b9fcc404206a3
zhou-liurong <19551230598@163.com>
2024-11-28 23:27:25 +0800

    stack和queue的课堂代码

--- stack_queue/stack_queue/test.cpp
+++ stack_queue/stack_queue/test.cpp
@@ -1,26 +1,156 @@
 
 #include<deque>
 #include <list>
-namespace bite
-{
-	template<class T, class Con = deque<T>>
-	//template<class T, class Con = list<T>>
-	class queue
-	{
-	public:
-		queue() {}
-		void push(const T& x) { _c.push_back(x); }
-		void pop() { _c.pop_front(); }
-		T& back() { return _c.back(); }
-		const T& back()const { return _c.back(); }
-		T& front() { return _c.front(); }
-		const T& front()const { return _c.front(); }
-		size_t size()const { return _c.size(); }
-		bool empty()const { return _c.empty(); }
-	private:
-		Con _c;
-	};
-}
+#include<vector>
+using namespace std;
+
+//int main()
+//{
+//	bit::priority_queue<int, vector<int>, Greater<int >> pq;
+//
+//
+//}
+
+//template<class T>
+//class Less
+//{
+//public:
+//	bool operator()(const T& x,const T& y)
+//	{
+//		return x < y;
+//	}
+//};
+//
+//template<class T>
+//class Greater
+//{
+//public:
+//	bool operator()(const T& x,const T& y)
+//	{
+//		return x > y;
+//	}
+//};
+//
+//namespace bit
+//{
+//	template<class T,class Container = vector<T>,class Compare = Less<T>>
+//	class priority_queue
+//	{
+//	public:
+//		void AdjustUp(int child)
+//		{
+//			Compare com;
+//			int parent = (child - 1) / 2;
+//			while (child > 0)
+//			{
+//				if(com(_con[parent],_con[child]))
+//				{
+//					swap(_con[child], _con[parent]);
+//					child = parent;
+//					parent = (child - 1) / 2;
+//				}
+//				else
+//				{
+//					break;
+//				}
+//			}
+//		}
+//
+//		void push(const T& x)
+//		{
+//			_con.push_back(x);
+//
+//			Adjustup(_con.size() - 1);
+//		}
+//
+//		void AdjustDown(int parent)
+//		{
+//			size_t child = parent * 2 + 1;
+//
+//			Compare com;
+//			while(child < _con.size())
+//			{
+//				if(child + 1 < _con.size() && com(_con[child],_con[child + 1]))
+//				{
+//					++child;
+//				}
+//
+//				if (com(_con[child], _con[parent]))
+//				{
+//					swap(_con[child], _con[parent]);
+//					parent = child;
+//					child = parent * 2 + 1£»
+//				}
+//				else
+//				{
+//					break;
+//				}
+//
+//			}
+//		}
+//	};
+//}
+//
+//
+//namespace bit
+//{
+//
+//
+//	//template<class T,class Container = deque<T>>
+//	//class stack
+//	//{
+//	//public:
+//	//	void push(const T& x)
+//	//	{
+//	//		_con.push_back(x);
+//	//	}
+//
+//	//	void pop()
+//	//	{
+//	//		_con.pop_back();
+//	//	}
+//	//	const T& top() const
+//	//	{
+//	//		return _con.back();
+//	//	}
+//
+//	//	size_t size() const
+//	//	{
+//	//		return _con.size();
+//	//	}
+//
+//	//	bool empty() const
+//	//	{
+//	//		return _con.empty();
+//	//	}
+//
+//	//private :
+//	//	Container _con;
+//	//};
+//}
+
+
+
+//namespace bite
+//{
+//	template<class T, class Con = deque<T>>
+//	//template<class T, class Con = list<T>>
+//	class queue
+//	{
+//	public:
+//		queue() {}
+//		void push(const T& x) { _c.push_back(x); }
+//		void pop() { _c.pop_front(); }
+//		T& back() { return _c.back(); }
+//		const T& back()const { return _c.back(); }
+//		T& front() { return _c.front(); }
+//		const T& front()const { return _c.front(); }
+//		size_t size()const { return _c.size(); }
+//		bool empty()const { return _c.empty(); }
+//	private:
+//		Con _c;
+//	};
+//}
 
 //#include<deque>
 //namespace bite
