b644d5b1339363986e198bc1994048a80cb845a7
zhou-liurong <19551230598@163.com>
2025-01-05 22:40:38 +0800

    两个数组的交集

---
+++ set_map/set_map/test.cpp
@@ -0,0 +1,24 @@
+
+//两个数组的交集
+//class Solution {
+//public:
+//    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+//        set<int> s1(nums1.begin(), nums1.end());
+//        set<int> s2(nums2.begin(), nums2.end());
+//        auto it1 = s1.begin();
+//        auto it2 = s2.begin();
+//        vector<int> ret;
+//        while (it1 != s1.end() && it2 != s2.end()) {
+//            if (*it1 > *it2)
+//                it2++;
+//            if (*it1 < *it2)
+//                it1++;
+//            if (*it1 == *it2) {
+//                ret.push_back(*it1);
+//                it1++;
+//                it2++;
+//            }
+//        }
+//        return ret;
+//    }
+//};
