c31665c843e0b56bdbe92c0852e817c8f1682367
zhou-liurong <19551230598@163.com>
2025-01-24 10:01:43 +0800

    二叉树的层序遍历

--- Binary_tree_advanced_algorithm_problem/Binary_tree_advanced_algorithm_problem/test.cpp
+++ Binary_tree_advanced_algorithm_problem/Binary_tree_advanced_algorithm_problem/test.cpp
@@ -1,4 +1,58 @@
 
+//二叉树的层序遍历
+///**
+// * Definition for a binary tree node.
+// * struct TreeNode {
+// *     int val;
+// *     TreeNode *left;
+// *     TreeNode *right;
+// *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// * };
+// */
+//class Solution {
+//public:
+//    vector<vector<int>> levelOrder(TreeNode* root) {
+//        vector<vector<int>> vv;
+//        queue<TreeNode*> q;
+//        int levelsize = 0;
+//
+//        if (root)
+//        {
+//            q.push(root);
+//            levelsize = 1;
+//        }
+//
+//        while (!q.empty())
+//        {
+//            vector<int> v;
+//            while (levelsize--)
+//            {
+//                TreeNode* front = q.front();
+//                q.pop();
+//                v.push_back(front->val);
+//
+//                if (front->left)
+//                {
+//                    q.push(front->left);
+//                }
+//
+//                if (front->right)
+//                {
+//                    q.push(front->right);
+//                }
+//
+//            }
+//
+//            vv.push_back(v);
+//            levelsize = q.size();
+//
+//        }
+//
+//        return vv;
+//    }
+//};
 //根据二叉树创建字符串
 ///**
 // * Definition for a binary tree node.
