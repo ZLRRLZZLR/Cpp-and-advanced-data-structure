99875f4c8202ad28437e64a306944e2b87c03208
zhou-liurong <19551230598@163.com>
2025-01-24 21:46:16 +0800

    二叉树的后序遍历非递归版本

--- Binary_tree_advanced_algorithm_problem/Binary_tree_advanced_algorithm_problem/test.cpp
+++ Binary_tree_advanced_algorithm_problem/Binary_tree_advanced_algorithm_problem/test.cpp
@@ -1,5 +1,43 @@
 
-
+//二叉树的后序遍历非递归版本
+///**
+// * Definition for a binary tree node.
+// * struct TreeNode {
+// *     int val;
+// *     TreeNode *left;
+// *     TreeNode *right;
+// *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// * };
+// */
+//class Solution {
+//public:
+//    vector<int> postorderTraversal(TreeNode* root) {
+//        stack<TreeNode*> st;
+//        vector<int> ret;
+//        TreeNode* cur = root;
+//        TreeNode* prev = nullptr;
+//        while (cur || st.size()) {
+//            while (cur) {
+//                st.push(cur);
+//                cur = cur->left;
+//            }
+//
+//            TreeNode* top = st.top();
+//            if (top->right == nullptr || top->right == prev) {
+//                ret.push_back(top->val);
+//                st.pop();
+//                prev = top;
+//            }
+//            else {
+//                cur = top->right;
+//            }
+//        }
+//
+//        return ret;
+//    }
+//};
 ///**二叉树的中序遍历非递归版本
 // * Definition for a binary tree node.
 // * struct TreeNode {
