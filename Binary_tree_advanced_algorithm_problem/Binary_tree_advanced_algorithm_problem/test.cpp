57ad84eb385adfe59f572b54f9eb1271cb5855eb
zhou-liurong <19551230598@163.com>
2025-01-24 17:31:24 +0800

    从后序与中序遍历序列构造二叉树

--- Binary_tree_advanced_algorithm_problem/Binary_tree_advanced_algorithm_problem/test.cpp
+++ Binary_tree_advanced_algorithm_problem/Binary_tree_advanced_algorithm_problem/test.cpp
@@ -1,4 +1,39 @@
-
+//从后序与中序遍历序列构造二叉树
+///**
+// * Definition for a binary tree node.
+// * struct TreeNode {
+// *     int val;
+// *     TreeNode *left;
+// *     TreeNode *right;
+// *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+// *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// * };
+// */
+//class Solution {
+//public:
+//    TreeNode* _buildTree(vector<int>& postorder, vector<int>& inorder, int& posti, int inbegin, int inend) {
+//        if (inbegin > inend)
+//            return nullptr;
+//
+//        TreeNode* root = new TreeNode(postorder[posti]);
+//
+//        int rooti = inbegin;
+//        while (inorder[rooti] != postorder[posti]) {
+//            rooti++;
+//        }
+//
+//        posti--;
+//
+//        root->right = _buildTree(postorder, inorder, posti, rooti + 1, inend);
+//        root->left = _buildTree(postorder, inorder, posti, inbegin, rooti - 1);
+//        return root;
+//    }
+//    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+//        int i = postorder.size() - 1;
+//        return _buildTree(postorder, inorder, i, 0, inorder.size() - 1);
+//    }
+//};
 //从前序与中序遍历序列构造二叉树
 ///**
 // * Definition for a binary tree node.
