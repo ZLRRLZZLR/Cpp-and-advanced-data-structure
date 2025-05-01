98e5064009aeedb1fad2d50d4f2273f9043bd707
zhou-liurong <19551230598@163.com>
2025-05-01 12:02:47 +0800

    位图和布隆过滤器的实现

--- Hash_extension/Hash_extension/BitSet.h
+++ Hash_extension/Hash_extension/BitSet.h
@@ -1,1 +1,103 @@
 #pragma once
+#include<vector>
+
+
+namespace bit
+{
+	template<size_t N>
+	class biteset
+	{
+	public:
+		biteset()
+		{
+			_bs.resize(N / 32 + 1);
+		}
+
+		void set(size_t x)
+		{
+			size_t i = x / 32;
+			size_t j = x % 32;
+
+			_bs[i] |= (1 << j);
+		}
+
+
+		void reset(size_t x)
+		{
+			size_t i = x / 32;
+			size_t j = x % 32;
+
+			_bs[i] &= (~(1 << j));
+		}
+
+
+		bool test(size_t x)
+		{
+			size_t i = x / 32;
+			size_t j = x % 32;
+
+			return _bs[i] &= (1 << j);
+		}
+
+
+	private:
+		std::vector<int> _bs;
+	};
+
+	template<size_t N>
+	class twobitset
+	{
+	public:
+		void set(size_t x)
+		{
+			bool bit1 = _bs1.test(x);
+			bool bit2 = _bs2.test(x);
+
+			if(!bit1 && !bit2)
+			{
+				_bs1.set(x);
+			}
+			else if (bit1 && !bit2)
+			{
+				_bs1.reset(x);
+				_bs2.set(x);
+			}
+			else if (!bit1 && bit2)
+			{
+				_bs1.set(x);
+			}
+			else
+			{
+				return;
+			}
+		}
+
+		int get_count(size_t x)
+		{
+			bool bit1 = _bs1.test(x);
+			bool bit2 = _bs2.test(x);
+
+			if (!bit1 && !bit2)
+			{
+				return 0;
+			}
+			else if (bit1 && !bit2)
+			{
+				return 1;
+			}
+			else if (!bit1 && bit2)
+			{
+				return 2;
+			}
+			else
+			{
+				return 3;
+			}
+		}
+
+	private:
+		biteset<N> _bs1;
+		biteset<N> _bs2;
+	};
+
+}
--- Hash_extension/Hash_extension/BloomFilter.h
+++ Hash_extension/Hash_extension/BloomFilter.h
@@ -1,1 +1,109 @@
 #pragma once
+#include<string>
+#include"BitSet.h"
+
+struct HashFuncBKDR
+{
+	// @detail 本 算法由于在Brian Kernighan与Dennis Ritchie的《The CProgramming Language》
+	// 一书被展示而得 名，是一种简单快捷的hash算法，也是Java目前采用的字符串的Hash算法累乘因子为31。
+	size_t operator()(const std::string& s)
+	{
+		size_t hash = 0;
+		for (auto ch : s)
+		{
+			hash *= 31;
+			hash += ch;
+		}
+		return hash;
+	}
+};
+
+
+struct HashFuncAP
+{
+	// 由Arash Partow发明的一种hash算法。  
+	size_t operator()(const std::string& s)
+	{
+		size_t hash = 0;
+		for (size_t i = 0; i < s.size(); i++)
+		{
+			if ((i & 1) == 0) // 偶数位字符
+			{
+				hash ^= ((hash << 7) ^ (s[i]) ^ (hash >> 3));
+			}
+			else              // 奇数位字符
+			{
+				hash ^= (~((hash << 11) ^ (s[i]) ^ (hash >> 5)));
+			}
+		}
+
+		return hash;
+	}
+};
+
+struct HashFuncDJB
+{
+	// 由Daniel J. Bernstein教授发明的一种hash算法。 
+	size_t operator()(const std::string& s)
+	{
+		size_t hash = 5381;
+		for (auto ch : s)
+		{
+			hash = hash * 33 ^ ch;
+		}
+
+		return hash;
+	}
+};
+
+template<size_t N,size_t X = 5,class K = std::string, 
+	class Hash1 = HashFuncBKDR,
+	class Hash2 = HashFuncAP,
+	class Hash3 = HashFuncDJB>
+class BloomFilter
+{
+public:
+	void Set(const K& key)
+	{
+		size_t hash1 = Hash1()(key) % M;
+		size_t hash2 = Hash2()(key) % M;
+		size_t hash3 = Hash3()(key) % M;
+
+		_bs.set(hash1);
+		_bs.set(hash2);
+		_bs.set(hash3);
+	}
+
+	bool Test(const K& key)
+	{
+		size_t hash1 = Hash1()(key) % M;
+		if(!_ba.test(hash1))
+		{
+			return false;
+		}
+		
+		size_t hash2 = Hash2()(key) % M;
+		if(!_bs.test(hash2))
+		{
+			return false;
+		}
+
+		size_t hash3 = Hash3()(key) % M;
+		if(!_bs.test(hash3))
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	double getFalseProbaility()
+	{
+		double p = pow(1.0 - pow(2.7, -3.0 / X), 3.0);
+
+		return p;
+	}
+private:
+	static const size_t M = N * X;
+	bit::bitset<M> _bs;
+};
