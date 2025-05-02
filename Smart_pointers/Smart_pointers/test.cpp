2144f531698fd6111a30d86893117f495ad80bcb
zhou-liurong <19551230598@163.com>
2025-05-02 16:41:32 +0800

    智能指针shared_ptr实现

--- Smart_pointers/Smart_pointers/test.cpp
+++ Smart_pointers/Smart_pointers/test.cpp
@@ -1,3 +1,163 @@
+#include<iostream>
+#include<memory>
+#include<functional>
+#include<atomic>
+using namespace std;
+
+namespace bit
+{
+	template<class T>
+	class shared_ptr
+	{
+	public:
+		shared_ptr(T* ptr)
+			:_ptr(ptr)
+			, _pcount(new int(1))
+		{}
+
+		template<class D>
+		shared_ptr(T* ptr, D del)
+			:_ptr(ptr)
+			, _pcount(new int(1))
+			,_del(del)
+		{ }
+
+		~shared_ptr()
+		{
+			if(--(*_pcount) == 0)
+			{
+				_del(_ptr);
+				delete _pcount;
+			}
+		}
+
+		shared_ptr(const shared_ptr<T>& sp)
+			:_ptr(sp._ptr)
+			,_pcount(sp._pcount)
+			,_del(sp._del)
+		{
+			(*_pcount)++;
+		}
+
+
+		shared_ptr<T>& operator=(const shared_ptr<T>& sp)
+		{
+			if(_ptr != sp._ptr)
+			{
+				if(--(*_pcount) == 0)
+				{
+					_del(_ptr);
+					delete(_pcount);
+				}
+				_ptr = sp._ptr;
+				_pcount = sp._pcount;
+				_del = sp._del;
+				++(*_pcount);
+			}
+
+			return *this;
+		}
+
+		T& operator*()
+		{
+			return *_ptr;
+		}
+
+
+		T* operator->()
+		{
+			return _ptr;
+		}
+
+
+		int use_count()
+		{
+			return *_pcount;
+		}
+
+	private:
+		T* _ptr;
+		int* _pcount;
+		function<void(T*)> _del = [](T*ptr) {delete ptr; };
+	};
+}
+
+
+//namespace bit
+//{
+//	template<class T>
+//	class shared_ptr
+//	{
+//	public:
+//		shared_ptr(T* ptr)
+//			:_ptr(ptr)
+//			, _pcount(new atomic<int>(1))
+//		{
+//		}
+//
+//		template<class D>
+//		shared_ptr(T* ptr £¬D del)
+//			: _ptr(ptr)
+//			, _pcount(new atomic<int>(1))
+//			, _del(del)
+//		{
+//		}
+//		~shared_ptr()
+//		{
+//			if(--(*_pcount) == 0)
+//			{
+//				_del(_ptr);
+//				delete _pcount;
+//			}
+//		}
+//
+//		shared_ptr(const shared_ptr<T>& sp)
+//			:_ptr(sp._ptr)
+//			, _pcount(sp.count)
+//		{
+//			(*_pcount)++;
+//		}
+//
+//		shared_ptr<T>& operator=(const shared_ptr<T>& sp)
+//		{
+//			if(_ptr != sp._ptr)
+//			{
+//				if(--(*_pcount) == 0)
+//				{
+//					delete _ptr;
+//					delete _pcount;
+//				}
+//
+//				_pcount = sp._pcount;
+//				_ptr = sp._str;
+//				++(*_pcount);
+//			}
+//
+//			return *this;
+//		}
+//		T& operator*()
+//		{
+//			return *_ptr;
+//		}
+//
+//		T* operator->()
+//		{
+//			return _ptr;
+//		}
+//
+//		int use_count()
+//		{
+//			return *_pcount;
+//		}
+//
+//	private:
+//		T* _ptr;
+//		int* _pcount;
+//
+//		functio<void(T*)> _del = [](T* ptr) {delete ptr; };
+//	};
+//}
+
 
 //#include<iostream>
 //#include<string>
