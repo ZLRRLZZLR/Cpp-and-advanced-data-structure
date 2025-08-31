3837a14898e076a905c3900097462d26269a3b10
zhou-liurong <19551230598@163.com>
2025-08-31 18:00:30 +0800

    并查集实现

--- C++11/C++11/test.cpp
+++ C++11/C++11/test.cpp
@@ -1,80 +1,214 @@
+#include<iostream>
+#include <functional>
 
+using namespace std;
 
-#include<functional>
 using placeholders::_1;
 using placeholders::_2;
 using placeholders::_3;
+
 int Sub(int a, int b)
 {
-	return (a - b) * 10;
+    return (a - b) * 10;
 }
+
 int SubX(int a, int b, int c)
 {
-	return (a - b - c) * 10;
+    return (a - b - c) * 10;
 }
+
 class Plus
 {
 public:
-	static int plusi(int a, int b)
-	{
-		return a + b;
-	}
-	double plusd(double a, double b)
-	{
-		return a + b;
-	}
+    static int plusi(int a, int b)
+    {
+        return a + b;
+    }
+    double plusd(double a, double b)
+    {
+        return a + b;
+    }
 };
+
 int main()
 {
-	auto sub1 = bind(Sub, _1, _2);
-	cout << sub1(10, 5) << endl;
-	// bind 本质返回的一个仿函数对象
-	// 调整参数顺序（不常用）
-	// _1代表第一个实参
-	// _2代表第二个实参
-	// ...
-	auto sub2 = bind(Sub, _2, _1);
-	out << sub2(10, 5) << endl;
-	// 调整参数个数 （常用）
-	auto sub3 = bind(Sub, 100, _1);
-	cout << sub3(5) << endl;
-	auto sub4 = bind(Sub, _1, 100);
-	cout << sub4(5) << endl;
-	// 分别绑死第123个参数
-	auto sub5 = bind(SubX, 100, _1, _2);
-	cout << sub5(5, 1) << endl;
-	auto sub6 = bind(SubX, _1, 100, _2);
-	cout << sub6(5, 1) << endl;
-	auto sub7 = bind(SubX, _1, _2, 100);
-	cout << sub7(5, 1) << endl;
-	// 成员函数对象进行绑死，就不需要每次都传递了
-	function<double(Plus&&, double, double)> f6 = &Plus::plusd;
-	Plus pd;
-	cout << f6(move(pd), 1.1, 1.1) << endl;
-	cout << f6(Plus(), 1.1, 1.1) << endl;
-	// bind一般用于，绑死一些固定参数
-	function<double(double, double)> f7 = bind(&Plus::plusd, Plus(), _1, _2);
-	cout << f7(1.1, 1.1) << endl;
-	// 计算复利的lambda
-	auto func1 = [](double rate, double money, int year)->double {
-		double ret = money;
-		for (int i = 0; i < year; i++)
-		{
-			ret += ret * rate;
-		}
-		return ret - money;
-		};
-	// 绑死一些参数，实现出支持不同年华利率，不同金额和不同年份计算出复利的结算利息
-	function<double(double)> func3_1_5 = bind(func1, 0.015, _1, 3);
-	function<double(double)> func5_1_5 = bind(func1, 0.015, _1, 5);
-	function<double(double)> func10_2_5 = bind(func1, 0.025, _1, 10);
-	function<double(double)> func20_3_5 = bind(func1, 0.035, _1, 30);
-	cout << func3_1_5(1000000) << endl;
-	cout << func5_1_5(1000000) << endl;
-	cout << func10_2_5(1000000) << endl;
-	cout << func20_3_5(1000000) << endl;
-	return 0;
+    auto sub1 = bind(Sub, _1, _2);
+    cout << sub1(10, 5) << endl;
+
+    // bind 本质返回的一个仿函数对象
+    // 调整参数顺序（不常用）
+    // _1代表第一个实参
+    // _2代表第二个实参
+    // ...
+    auto sub2 = bind(Sub, _2, _1);
+    cout << sub2(10, 5) << endl;
+
+    // 调整参数个数 （常用）
+    auto sub3 = bind(Sub, 100, _1);
+    cout << sub3(5) << endl;
+    auto sub4 = bind(Sub, _1, 100);
+    cout << sub4(5) << endl;
+
+    // 分别绑死第123个参数
+    auto sub5 = bind(SubX, 100, _1, _2);
+    cout << sub5(5, 1) << endl;
+    auto sub6 = bind(SubX, _1, 100, _2);
+    cout << sub6(5, 1) << endl;
+    auto sub7 = bind(SubX, _1, _2, 100);
+    cout << sub7(5, 1) << endl;
+
+    // 成员函数对象进行绑死，就不需要每次都传递了
+    function<double(Plus&&, double, double)> f6 = &Plus::plusd;
+    Plus pd;
+    cout << f6(move(pd), 1.1, 1.1) << endl;
+    cout << f6(Plus(), 1.1, 1.1) << endl;
+
+    // bind一般用于，绑死一些固定参数
+    function<double(double, double)> f7 = bind(&Plus::plusd, Plus(), _1, _2);
+    cout << f7(1.1, 1.1) << endl;
+    // 计算复利的lambda
+    auto func1 = [](double rate, double money, int year) -> double
+        {
+            double ret = money;
+            for (int i = 0; i < year; i++)
+            {
+                ret += ret * rate;
+            }
+            return ret - money;
+        };
+
+    // 绑死一些参数，实现出支持不同年华利率，不同金额和不同年份计算出复利的结算利息
+    function<double(double)> func3_1_5 = bind(func1, 0.015, _1, 3);
+    function<double(double)> func5_1_5 = bind(func1, 0.015, _1, 5);
+    function<double(double)> func10_2_5 = bind(func1, 0.025, _1, 10);
+    function<double(double)> func20_3_5 = bind(func1, 0.035, _1, 30);
+    cout << func3_1_5(1000000) << endl;
+    cout << func5_1_5(1000000) << endl;
+    cout << func10_2_5(1000000) << endl;
+    cout << func20_3_5(1000000) << endl;
+
+
+    return 0;
 }
+
+//struct Goods
+//{
+//    string _name;  // 名字
+//    double _price; // 价格
+//    int _evaluate; // 评价
+//    // ...
+//    Goods(const char* str, double price, int evaluate)
+//        : _name(str), _price(price), _evaluate(evaluate)
+//    {
+//    }
+//};
+//struct ComparePriceLess
+//{
+//    bool operator()(const Goods& gl, const Goods& gr)
+//    {
+//        return gl._price < gr._price;
+//    }
+//};
+//
+//struct ComparePriceGreater
+//{
+//    bool operator()(const Goods& gl, const Goods& gr) { return gl._price > gr._price; }
+//};
+//
+//int main()
+//{
+//    vector<Goods> v = { {"苹果", 2.1, 5}, {"香蕉", 3, 4}, {"橙子", 2.2, 3}, {"菠萝", 1.5, 4} };
+//    // 类似这样的场景，我们实现仿函数对象或者函数指针支持商品中
+//    // 不同项的比较，相对还是比较麻烦的，那么这里lambda就很好用了
+//    sort(v.begin(), v.end(), ComparePriceLess());
+//    sort(v.begin(), v.end(), ComparePriceGreater());
+//    sort(v.begin(), v.end(), [](const Goods& g1, const Goods& g2)
+//        { return g1._price < g2._price; });
+//    sort(v.begin(), v.end(), [](const Goods& g1, const Goods& g2)
+//        { return g1._price > g2._price; });
+//    sort(v.begin(), v.end(), [](const Goods& g1, const Goods& g2)
+//        { return g1._evaluate < g2._evaluate; });
+//    sort(v.begin(), v.end(), [](const Goods& g1, const Goods& g2)
+//        { return g1._evaluate > g2._evaluate; });
+//    return 0;
+//}
+
+
+//#include<functional>
+//using placeholders::_1;
+//using placeholders::_2;
+//using placeholders::_3;
+//int Sub(int a, int b)
+//{
+//	return (a - b) * 10;
+//}
+//int SubX(int a, int b, int c)
+//{
+//	return (a - b - c) * 10;
+//}
+//class Plus
+//{
+//public:
+//	static int plusi(int a, int b)
+//	{
+//		return a + b;
+//	}
+//	double plusd(double a, double b)
+//	{
+//		return a + b;
+//	}
+//};
+//int main()
+//{
+//	auto sub1 = bind(Sub, _1, _2);
+//	cout << sub1(10, 5) << endl;
+//	// bind 本质返回的一个仿函数对象
+//	// 调整参数顺序（不常用）
+//	// _1代表第一个实参
+//	// _2代表第二个实参
+//	// ...
+//	auto sub2 = bind(Sub, _2, _1);
+//	out << sub2(10, 5) << endl;
+//	// 调整参数个数 （常用）
+//	auto sub3 = bind(Sub, 100, _1);
+//	cout << sub3(5) << endl;
+//	auto sub4 = bind(Sub, _1, 100);
+//	cout << sub4(5) << endl;
+//	// 分别绑死第123个参数
+//	auto sub5 = bind(SubX, 100, _1, _2);
+//	cout << sub5(5, 1) << endl;
+//	auto sub6 = bind(SubX, _1, 100, _2);
+//	cout << sub6(5, 1) << endl;
+//	auto sub7 = bind(SubX, _1, _2, 100);
+//	cout << sub7(5, 1) << endl;
+//	// 成员函数对象进行绑死，就不需要每次都传递了
+//	function<double(Plus&&, double, double)> f6 = &Plus::plusd;
+//	Plus pd;
+//	cout << f6(move(pd), 1.1, 1.1) << endl;
+//	cout << f6(Plus(), 1.1, 1.1) << endl;
+//	// bind一般用于，绑死一些固定参数
+//	function<double(double, double)> f7 = bind(&Plus::plusd, Plus(), _1, _2);
+//	cout << f7(1.1, 1.1) << endl;
+//	// 计算复利的lambda
+//	auto func1 = [](double rate, double money, int year)->double {
+//		double ret = money;
+//		for (int i = 0; i < year; i++)
+//		{
+//			ret += ret * rate;
+//		}
+//		return ret - money;
+//		};
+//	// 绑死一些参数，实现出支持不同年华利率，不同金额和不同年份计算出复利的结算利息
+//	function<double(double)> func3_1_5 = bind(func1, 0.015, _1, 3);
+//	function<double(double)> func5_1_5 = bind(func1, 0.015, _1, 5);
+//	function<double(double)> func10_2_5 = bind(func1, 0.025, _1, 10);
+//	function<double(double)> func20_3_5 = bind(func1, 0.035, _1, 30);
+//	cout << func3_1_5(1000000) << endl;
+//	cout << func5_1_5(1000000) << endl;
+//	cout << func10_2_5(1000000) << endl;
+//	cout << func20_3_5(1000000) << endl;
+//	return 0;
+//}
 
 //simple(1)
 //template <class Fn, class... Args>
@@ -1648,3 +1782,5 @@ int main()
 ////	Point p = { 1, 2 };
 ////	return 0;
 ////}
+
+
