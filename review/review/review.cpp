a1b848ef6bf1e67d7181ddae41b6d37e568f9c18
zhou-liurong <19551230598@163.com>
2025-11-19 20:37:33 +0800

    [复习日志]LRU实现

--- review/review/review.cpp
+++ review/review/review.cpp
@@ -13,13 +13,41 @@ public:
 	}
 
 	int get(int key) {
-
-
+		auto hashit = _hashmap.find(key);
+		if (hashit != _hashmap.end()) {
+			auto listit = hashit->second;
+			pair<int, int> value = *listit;
+			_list.erase(listit);
+			_list.push_front(value);
+			_hashmap[key] = _list.begin();
+			return value.first;
+		}
+		else {
+			return -1;
+		}
 
 	}
 
 	void put(int key,int value) {
+		auto hashit = _hashmap.find(key);
+		if (hashit != _hashmap.end()) {
+			auto listit = hashit->second;
+			pair<int, int> value = *listit;
+			_list.erase(listit);
+			_list.push_front(value);
+			_hashmap[key] = _list.begin();
 
+		}
+		else {
+			pair<int, int> listit = _list.back();
+			if (_hashmap.size() >= _capacity)
+			{
+				_list.pop_back();
+				_hashmap.erase(listit.first);
+			}
+			_list.push_front(make_pair(key, value));
+			_hashmap[key] = _list.begin();
+		}
 
 	}
 
@@ -27,7 +55,7 @@ public:
 private:
 	list<pair<int, int>> _list;
 	size_t _capacity;
-	unordered_map<int, list<int, int>::iterator> _hashmap;
+	unordered_map<int, list<pair<int, int>>::iterator> _hashmap;
 };
 
 
